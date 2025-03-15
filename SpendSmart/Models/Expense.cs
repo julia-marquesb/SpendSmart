05ab7ff2a3803e1626db65a62921c6133b6eaa32
Julia Marques <julia.marquesb@gmail.com>
2025-03-15 12:06:30 -0400

    first commit

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required] //there must be a description
+        public string? Description { get; set; }
+    }
+}
---
+++ SpendSmart/Models/SpendSmartExpensesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartExpensesDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartExpensesDbContext(DbContextOptions<SpendSmartExpensesDbContext> options)
+            : base(options) 
+        {
+            
+        }
+    }
+}
